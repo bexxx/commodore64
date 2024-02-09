64e0daf1aa1919306918739de864dc2435a15e1d
Ralf "bexxx" Beckers <me@bexxx.de>
2024-02-09 12:58:40 +0100

    add stupid dump tool. I hate pyton, I wanna die

---
+++ tools/dump_bytes.py
@@ -0,0 +1,21 @@
+import sys
+ 
+file = open(sys.argv[1], "rb")
+
+
+data = file.read(2)
+
+data = file.read(16)
+
+# Printing data by iterating with while loop
+while data:
+    print ("    .byte ", end= "")
+    for d in data:
+#        i = ord(d)   # Get the integer value of the byte
+        hex = "${0:x}, ".format(d) # hexadecimal: ff    
+        print(hex, end="")
+    print("")
+    data = file.read(16)
+
+# Close the binary file
+file.close()
