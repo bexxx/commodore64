6e51f8176b6c5d4e7c33f107ebfa8b371627976a
Ralf "bexxx" Beckers <me@bexxx.de>
2026-02-22 15:56:19 +0100

    Finalize code gen version

--- getting-started/D Line Duplication/RepeatBadlineRelaxRest.asm
+++ getting-started/D Line Duplication/RepeatBadlineRelaxRest.asm
@@ -3,10 +3,17 @@
 #import "../../includes/cia2_constants.inc"
 #import "../../includes/internals.inc"
 #import "../../includes/zeropage.inc"
-#import "../../includes/BezierEasings.asm"
-
 
 .filenamespace SimpleD011Stretcher
+
+// based on Trident's / FailightTV video "$D011 Mayhem". This is the cheap stretcher.
+// YT: https://www.youtube.com/watch?v=0KhH4YrUsXo
+
+// uncomment to fill screen with std font characters instead of logo
+//#define USE_ROM_FONT
+
+// uncomment to show border indicator for stretch value compute time indicator
+//#define SHOW_TIMING
 
 .namespace Configuration {
     .label RasterLineIrqSetup = $20             // start line for raster interrupt
@@ -57,10 +64,13 @@ BasicUpstart2(main)
 
     cli
 
-    //jsr fillScreen
+#if USE_ROM_FONT
+    jsr fillScreen
+#else
     jsr copyLogo
-!:    jmp !-
+#endif 
 
+!:    jmp !-
 
 .align $100                                     // align on the start of a new page
 .segment Default "raster interrupt"             // shows the bytes for this code, when using -showmem
@@ -122,7 +132,7 @@ fixcycle:
 .align $80
 irqStretcherStart: {
 row: 
-    .for (var i=0; i<25; i++) {
+    .for (var i=0; i<25; i++) {     // generate this codeblock 25 times for each char row. Instead of codegen, this block can be reused, when updating the stretch value at the end of each row.
         SaveIrqRegistersZPWithTimer(Configuration.IrqStretchAccuZpLocation, Configuration.IrqStretchXRegZpLocation, Configuration.IrqStretchYRegZpLocation)
         nop             // 2: 57
 
@@ -148,14 +158,14 @@ row:
         bcc !+ 
         jmp endFrame
 !:
-        .fill 10, NOP
-        lda $d012
+        .fill 10, NOP   // still within the badline, make sure, we are on the next raster line when loading current line
+        lda $d012       // calculate line for next raster irq
         adc #6
         bcc !+
         jmp endFrame
 !:      cmp #247
         bcc !+
-quit:        jmp endFrame
+quit:   jmp endFrame
 !:
         sta irqwait.irqset.rasterLine
         irqwait: irq_wait(
@@ -168,7 +178,10 @@ quit:        jmp endFrame
 
 endFrame:
 
-//inc $d020
+#if SHOW_TIMING
+    inc $d020
+#endif 
+
     lda #0
     sta $20
     clc
@@ -177,11 +190,10 @@ endFrame:
     .for (var i=0; i<25; i++) {
         sta irqStretcherStart.row[i].stretch
     }
-.break
     .for (var i=0; i<25; i++) {
         lda SineTable120,x
-        sec
-        sbc SineTable95,y
+        clc
+        adc SineTable95,y
         sta irqStretcherStart.row[i].stretch
         adc $20
         bcc !+
@@ -203,7 +215,7 @@ endFrame:
     !:
     }
 endStretchUpdate:
-//dec $d020
+
        dec sine1Index
         bne !+
         ldx #(SineTable120End - SineTable120 - 1)
@@ -217,6 +229,9 @@ endStretchUpdate:
     lda #%00011011
     sta $d011
 
+#if SHOW_TIMING
+    dec $d020
+#endif 
 
     lda #<irqStretcherStart
     sta Internals.InterruptHandlerPointerRomLo
@@ -224,7 +239,6 @@ endStretchUpdate:
     sta Internals.InterruptHandlerPointerRomHi
     lda #Configuration.StartStretchIrqRasterLine
     sta $d012
-
 
     lsr VIC.INTERRUPT_EVENT                     // 6: 25 ack raster irq
     
@@ -235,22 +249,19 @@ endStretchUpdate:
     rti
 }
 
-// python3 .\sinusgen.py -type 16 -min 0 -max 23 -steps 255 -previewfile test.png -show
-//stretchValues:
-//    .import binary "output.bin"
-
 .align $100
 .segment Default "sine table source"
-stretchValues:
 SineTable120:
-    .fill 60, 9+ (9 * sin(toRadians((i*360)/(60)))) 
+    .const sine1_length = 60
+    .fill sine1_length, 5 + (5 * sin(toRadians((i*360)/(sine1_length)))) 
 SineTable120End:
 
 SineTable95:
-    .fill 70, 9+ (9 * sin(toRadians((i*360)/(70)))) 
+    .const sine2_length = 75
+    .fill sine2_length, 5 + (5 * sin(toRadians((i*360)/(sine2_length)))) 
 SineTable95End:
 
-
+#if !USE_ROM_FONT
 copyLogo:
     ldx #0
 !:  lda screen + $000,x
@@ -280,6 +291,7 @@ copyLogo:
     bne !-
     rts
 
+#else
 
 fillScreen:
     ldy #25
@@ -302,6 +314,7 @@ fillScreen:
     dey
     bne !---
     rts
+#endif 
 
 .macro SaveIrqRegistersZPWithTimer (AccuZpLocation, XRegZpLocation, YRegZpLocation) {
     sta AccuZpLocation        // 2: 9 + jitter
@@ -366,4 +379,63 @@ next:
     lda AccuZpLocation
 }
 
-#import "slackers.asm"
+#if !USE_ROM_FONT
+screen_001:
+.byte 0,0
+
+*=$3000
+screen:
+	.byte	$A0, $A0, $A0, $A0, $A0, $A0, $A0, $A0, $A0, $A0, $A0, $A0, $A0, $A0, $A0, $A0, $A0, $A0, $A0, $A0, $A0, $A0, $A0, $A0, $A0, $A0, $A0, $A0, $A0, $A0, $A0, $A0, $A0, $A0, $A0, $A0, $A0, $A0, $A0, $A0
+	.byte	$61, $6F, $6F, $6F, $02, $5F, $A0, $A0, $20, $6F, $6F, $6F, $05, $E2, $20, $6F, $6F, $6F, $6F, $6F, $6F, $6F, $33, $E1, $A0, $A0, $A0, $69, $20, $6F, $6F, $18, $E1, $61, $20, $6F, $6F, $6F, $20, $E1
+	.byte	$61, $F4, $F0, $A0, $CD, $20, $5F, $A0, $75, $F4, $F0, $EA, $CD, $20, $5F, $A0, $A0, $A0, $A0, $A0, $A0, $EE, $A0, $E1, $A0, $A0, $69, $E9, $A0, $A0, $EE, $A0, $E1, $61, $A0, $A0, $A0, $EE, $A0, $E1
+	.byte	$61, $F4, $C2, $A0, $A0, $CD, $20, $5F, $75, $F4, $C2, $EA, $A0, $CD, $20, $5F, $A0, $A0, $A0, $A0, $A0, $C2, $A0, $E1, $A0, $69, $20, $A0, $A0, $A0, $C2, $A0, $E1, $7E, $E9, $A0, $A0, $C2, $A0, $E1
+	.byte	$61, $F4, $A1, $A0, $A0, $A0, $CD, $20, $7E, $F4, $A1, $EA, $A0, $A0, $CD, $20, $6C, $F8, $F8, $20, $A0, $A1, $A0, $E1, $69, $20, $A0, $A0, $69, $F4, $A1, $A0, $7C, $E9, $A0, $A0, $A0, $A1, $A0, $E1
+	.byte	$61, $F4, $AE, $EA, $5F, $A0, $A0, $CD, $20, $F4, $AE, $EA, $5F, $A0, $A0, $CD, $7C, $A0, $69, $E9, $A0, $AE, $A0, $7C, $20, $A0, $A0, $69, $2F, $F4, $AE, $A0, $E9, $A0, $A0, $CE, $69, $AE, $A0, $E1
+	.byte	$61, $F4, $A0, $EA, $DF, $5F, $A0, $A0, $DF, $F4, $A0, $E7, $DF, $5F, $A0, $A0, $DF, $7C, $E9, $A0, $A0, $CE, $69, $E9, $A0, $A0, $A0, $A0, $69, $F4, $A0, $AF, $A0, $A0, $CE, $69, $20, $A0, $FD, $E1
+	.byte	$61, $F4, $A0, $EA, $E2, $E9, $A0, $CE, $69, $F4, $A0, $E7, $E2, $E9, $A0, $CE, $69, $E9, $A0, $A0, $CE, $69, $79, $79, $79, $79, $79, $79, $7B, $F4, $AF, $A0, $A0, $CE, $69, $E9, $62, $62, $68, $FF
+	.byte	$61, $F4, $A0, $EA, $A0, $A0, $A0, $69, $E9, $F4, $A0, $A0, $A0, $A0, $CE, $69, $E9, $A0, $A0, $CE, $69, $E9, $A0, $A0, $A0, $A0, $A0, $A0, $20, $AF, $A0, $A0, $CE, $69, $E9, $A0, $A0, $A0, $A1, $FE
+	.byte	$61, $F4, $A0, $E7, $A0, $A0, $DF, $E1, $A0, $F4, $A0, $A0, $A0, $CE, $69, $E9, $A0, $A0, $CE, $69, $E9, $A0, $A0, $A0, $A0, $A0, $A0, $69, $E9, $A0, $A0, $CE, $69, $E9, $A0, $A0, $A0, $A0, $A0, $A0
+	.byte	$61, $F4, $A0, $EA, $A0, $A0, $A0, $DF, $5F, $F4, $A0, $A0, $CE, $69, $E9, $A0, $A0, $CE, $69, $E1, $A0, $A0, $A0, $A0, $A0, $A0, $69, $E9, $A0, $A0, $CE, $AF, $67, $A0, $A0, $A0, $A0, $A0, $A0, $A0
+	.byte	$61, $F4, $A0, $EA, $5F, $A0, $A0, $A0, $DF, $F4, $A0, $A0, $69, $F8, $5F, $A0, $A0, $A0, $CD, $7C, $FB, $A0, $A0, $A0, $A0, $69, $E9, $A0, $A0, $CE, $AF, $E7, $E9, $A0, $A0, $A0, $A0, $A0, $A0, $A0
+	.byte	$61, $F4, $A0, $EA, $DF, $5F, $A0, $A0, $AE, $E5, $A0, $EA, $4E, $E2, $7E, $5F, $EF, $A0, $A0, $CD, $7C, $FB, $A0, $A0, $69, $E9, $A0, $A0, $CE, $AF, $A0, $EA, $69, $78, $78, $78, $78, $78, $AE, $A0
+	.byte	$61, $F4, $A0, $EA, $69, $E9, $A0, $CE, $69, $F4, $A0, $EA, $E9, $A0, $A0, $AE, $CE, $A0, $A0, $A0, $CD, $7C, $A0, $69, $E9, $A0, $A0, $CE, $69, $F4, $A0, $EA, $E9, $A0, $A0, $AE, $69, $E9, $FB, $A0
+	.byte	$61, $F4, $A0, $EA, $E9, $A0, $CE, $69, $7B, $F4, $A0, $EA, $E9, $A0, $CE, $69, $79, $5F, $A0, $A0, $A0, $DF, $7E, $E9, $A0, $A0, $CE, $69, $7B, $F4, $A0, $EA, $E9, $A0, $CE, $69, $E9, $FC, $AE, $A0
+	.byte	$61, $F4, $A0, $A0, $A0, $CE, $69, $E9, $61, $F4, $A0, $A0, $A0, $CE, $69, $E9, $69, $E9, $A0, $A0, $AE, $69, $E9, $A0, $A0, $CE, $69, $E9, $61, $F4, $A0, $A0, $A0, $CE, $69, $E9, $A0, $A0, $A0, $A0
+	.byte	$61, $F4, $A0, $A0, $A0, $69, $E9, $A0, $61, $F4, $A0, $A0, $A0, $69, $E9, $69, $E9, $A0, $A0, $CE, $69, $E9, $A0, $A0, $A0, $69, $E9, $A0, $61, $F4, $A0, $A0, $A0, $69, $E9, $A0, $A0, $A0, $A0, $A0
+	.byte	$61, $F4, $A0, $A0, $20, $E9, $A0, $A0, $61, $F4, $A0, $A0, $20, $E9, $69, $20, $A0, $A0, $CE, $69, $7B, $F4, $A0, $A0, $34, $E9, $A0, $A0, $61, $F4, $A0, $A0, $20, $E9, $A0, $A0, $A0, $A0, $A0, $A0
+	.byte	$61, $F4, $A0, $20, $E9, $A0, $A0, $A0, $61, $F4, $A0, $20, $E9, $69, $20, $A0, $A0, $CE, $69, $E9, $61, $F4, $A0, $32, $E9, $A0, $A0, $A0, $61, $F4, $A0, $20, $E9, $A0, $A0, $A0, $A0, $A0, $A0, $A0
+	.byte	$61, $F4, $20, $E9, $A0, $A0, $A0, $A0, $61, $F4, $20, $E9, $69, $20, $A0, $A0, $CE, $69, $E9, $A0, $61, $F4, $30, $E9, $A0, $A0, $A0, $A0, $61, $F4, $20, $E9, $A0, $A0, $A0, $A0, $A0, $A0, $A0, $A0
+	.byte	$61, $69, $E9, $A0, $A0, $A0, $A0, $A0, $20, $69, $E9, $69, $20, $A0, $A0, $CE, $69, $E9, $A0, $A0, $32, $69, $E9, $A0, $A0, $A0, $A0, $AC, $20, $69, $E9, $AE, $A0, $A0, $A0, $A0, $A0, $A0, $A0, $A0
+	.byte	$61, $E9, $AE, $A0, $A0, $A0, $A0, $A0, $7B, $E9, $69, $20, $A0, $A0, $CE, $69, $E9, $A0, $A0, $BA, $7B, $E9, $AE, $A0, $A0, $A0, $A0, $EC, $7B, $E9, $AC, $A0, $A0, $A0, $A0, $A0, $A0, $A0, $A0, $A0
+	.byte	$7F, $EC, $A0, $A0, $A0, $A0, $A0, $A0, $A1, $69, $20, $A0, $A0, $CE, $69, $E9, $A0, $A0, $A0, $AE, $DA, $7F, $A0, $A0, $A0, $A0, $A0, $AE, $C8, $DA, $A0, $A0, $A0, $A0, $A0, $A0, $A0, $A0, $A0, $A0
+	.byte	$DA, $A0, $A0, $A0, $A0, $A0, $A0, $AE, $69, $E9, $EF, $EF, $EF, $69, $E9, $A0, $A0, $A0, $A0, $A0, $AE, $A0, $A0, $A0, $A0, $A0, $A0, $A0, $A1, $FB, $A0, $A0, $A0, $A0, $A0, $A0, $A0, $A0, $A0, $A0
+	.byte	$AE, $A0, $A0, $A0, $A0, $A0, $A0, $EC, $62, $62, $62, $62, $62, $62, $FB, $A0, $A0, $A0, $A0, $A0, $A0, $A0, $A0, $A0, $A0, $A0, $A0, $A0, $AE, $A0, $A0, $A0, $A0, $A0, $A0, $A0, $A0, $A0, $A0, $A0
+
+*=$4000
+colors:
+	.byte	$05, $05, $05, $05, $05, $05, $03, $05, $05, $05, $05, $05, $05, $05, $05, $05, $05, $05, $05, $05, $05, $05, $05, $05, $03, $0E, $03, $05, $05, $05, $05, $05, $05, $05, $05, $05, $05, $05, $05, $05
+	.byte	$05, $01, $01, $01, $0B, $05, $05, $05, $0E, $01, $01, $01, $0B, $05, $01, $01, $01, $01, $01, $01, $01, $01, $0B, $05, $03, $03, $05, $05, $0B, $01, $01, $0B, $05, $05, $0B, $01, $01, $01, $0B, $05
+	.byte	$05, $01, $01, $01, $0F, $01, $05, $05, $05, $01, $01, $01, $0F, $01, $01, $01, $01, $01, $01, $01, $01, $01, $01, $05, $03, $05, $05, $01, $01, $01, $01, $01, $05, $05, $01, $01, $01, $07, $07, $05
+	.byte	$05, $01, $01, $01, $01, $0F, $05, $05, $05, $01, $01, $01, $01, $0F, $01, $01, $01, $01, $01, $01, $01, $01, $01, $05, $05, $05, $01, $01, $01, $01, $01, $01, $05, $05, $01, $01, $07, $07, $07, $05
+	.byte	$05, $01, $01, $01, $01, $01, $0F, $01, $05, $01, $01, $01, $01, $01, $0F, $01, $05, $05, $05, $01, $01, $01, $07, $05, $05, $01, $01, $01, $01, $01, $01, $01, $05, $01, $01, $07, $07, $07, $07, $05
+	.byte	$05, $01, $01, $01, $01, $01, $01, $0F, $01, $01, $01, $01, $01, $01, $01, $0F, $05, $05, $05, $01, $01, $07, $07, $05, $01, $01, $01, $01, $05, $01, $01, $01, $01, $01, $07, $07, $07, $07, $07, $05
+	.byte	$05, $01, $01, $01, $05, $01, $01, $01, $01, $01, $01, $01, $05, $01, $01, $01, $01, $05, $01, $01, $07, $07, $07, $01, $01, $01, $01, $01, $01, $01, $01, $01, $01, $07, $07, $07, $01, $07, $07, $05
+	.byte	$05, $01, $01, $01, $05, $01, $01, $01, $01, $01, $01, $01, $05, $01, $01, $01, $01, $01, $01, $07, $07, $07, $05, $05, $05, $05, $05, $05, $05, $01, $01, $01, $07, $07, $07, $05, $05, $05, $05, $05
+	.byte	$05, $01, $01, $01, $01, $01, $01, $01, $05, $01, $01, $01, $01, $01, $01, $01, $01, $01, $07, $07, $07, $05, $05, $03, $03, $03, $03, $05, $01, $01, $01, $07, $07, $07, $05, $05, $03, $03, $03, $03
+	.byte	$05, $01, $01, $01, $01, $01, $01, $05, $05, $01, $01, $01, $01, $01, $01, $01, $01, $07, $07, $07, $05, $03, $03, $0E, $0E, $03, $05, $05, $01, $01, $07, $07, $07, $05, $05, $03, $0E, $0E, $0E, $0E
+	.byte	$05, $01, $01, $01, $01, $01, $01, $01, $05, $01, $01, $01, $01, $01, $01, $01, $07, $07, $07, $05, $03, $03, $0E, $0E, $03, $05, $05, $01, $01, $07, $07, $07, $05, $05, $03, $0E, $0E, $0E, $0E, $0E
+	.byte	$05, $01, $01, $01, $01, $01, $01, $01, $01, $01, $01, $01, $01, $05, $01, $07, $07, $07, $0F, $05, $05, $03, $03, $03, $05, $05, $01, $01, $07, $07, $07, $01, $05, $05, $03, $03, $03, $03, $03, $0E
+	.byte	$05, $01, $01, $01, $05, $01, $01, $07, $07, $01, $01, $01, $05, $05, $05, $07, $07, $07, $01, $0F, $05, $05, $03, $05, $05, $01, $01, $07, $07, $07, $01, $01, $05, $05, $05, $05, $05, $05, $05, $03
+	.byte	$05, $01, $01, $01, $05, $01, $07, $07, $07, $01, $01, $01, $01, $01, $07, $07, $07, $01, $01, $01, $0F, $05, $05, $05, $01, $01, $07, $07, $07, $01, $01, $01, $01, $01, $07, $07, $07, $05, $05, $03
+	.byte	$05, $01, $01, $01, $01, $07, $07, $07, $05, $01, $01, $01, $01, $07, $07, $07, $05, $01, $01, $01, $07, $07, $05, $01, $01, $07, $07, $07, $05, $01, $01, $01, $01, $07, $07, $07, $05, $05, $03, $03
+	.byte	$05, $01, $01, $01, $07, $07, $07, $05, $05, $01, $01, $01, $07, $07, $07, $05, $05, $01, $01, $07, $07, $07, $01, $01, $07, $07, $07, $05, $05, $01, $01, $01, $07, $07, $07, $05, $05, $03, $03, $0E
+	.byte	$05, $01, $01, $07, $07, $07, $05, $05, $05, $01, $01, $07, $07, $07, $05, $05, $01, $01, $07, $07, $07, $01, $01, $07, $07, $07, $05, $05, $05, $01, $01, $07, $07, $07, $05, $05, $03, $03, $0E, $0E
+	.byte	$05, $01, $07, $07, $01, $05, $05, $03, $05, $01, $07, $07, $01, $05, $05, $01, $01, $07, $07, $07, $05, $01, $07, $07, $0B, $05, $05, $03, $05, $01, $07, $07, $01, $05, $05, $03, $03, $0E, $0E, $0E
+	.byte	$05, $07, $07, $01, $05, $05, $03, $03, $05, $07, $07, $01, $05, $05, $0E, $01, $07, $07, $07, $05, $05, $07, $07, $0B, $05, $03, $03, $03, $05, $07, $07, $01, $05, $05, $03, $03, $0E, $0E, $0E, $0E
+	.byte	$05, $07, $01, $05, $05, $03, $03, $05, $05, $07, $01, $05, $05, $0E, $01, $07, $07, $07, $05, $05, $05, $07, $0B, $05, $05, $03, $03, $05, $05, $07, $01, $05, $05, $03, $03, $0E, $0E, $0E, $0E, $0E
+	.byte	$05, $07, $05, $05, $03, $03, $03, $05, $05, $07, $05, $05, $0E, $01, $07, $07, $07, $05, $05, $05, $0B, $07, $05, $05, $03, $03, $03, $05, $0E, $07, $05, $05, $03, $03, $0E, $0E, $0E, $0E, $0E, $0E
+	.byte	$05, $05, $05, $03, $03, $0E, $03, $05, $05, $05, $05, $0E, $01, $07, $07, $07, $05, $05, $03, $05, $05, $05, $05, $03, $03, $0E, $03, $05, $05, $05, $05, $03, $03, $0E, $0E, $0E, $0E, $0E, $0E, $0E
+	.byte	$05, $05, $03, $03, $0E, $0E, $03, $03, $05, $05, $0E, $01, $07, $07, $07, $05, $05, $03, $03, $05, $05, $05, $03, $03, $0E, $0E, $03, $03, $05, $05, $03, $03, $0E, $0E, $0E, $0E, $0E, $0E, $0E, $0E
+	.byte	$03, $03, $03, $0E, $0E, $0E, $03, $05, $05, $01, $01, $07, $07, $07, $05, $05, $03, $03, $0E, $03, $05, $03, $03, $0E, $0E, $0E, $0E, $03, $03, $03, $03, $0E, $0E, $0E, $0E, $0E, $0E, $0E, $0E, $0E
+	.byte	$0E, $0E, $0E, $0E, $0E, $0E, $03, $05, $05, $05, $05, $05, $05, $05, $05, $03, $03, $0E, $0E, $03, $03, $03, $0E, $0E, $0E, $0E, $0E, $0E, $0E, $0E, $0E, $0E, $0E, $0E, $0E, $0E, $0E, $0E, $0E, $0E
+#endif 
